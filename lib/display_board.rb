06950e8d857074b565b18932c9ce664937fb3f98
Raffi Jebejian <raffi.jebejian@gmail.com>
2017-10-26 05:51:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board
-  board = [" ","X","O"]
+  board = ["X"," ","O"]
   puts ["#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}", "#{board[0]}"]
 
 end
