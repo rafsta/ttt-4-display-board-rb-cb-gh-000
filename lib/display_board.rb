651b6b221bd4093bdcf87cb2a6e03fee1898d0b5
Raffi Jebejian <raffi.jebejian@gmail.com>
2017-10-26 05:38:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,8 @@
 # out the current state.
 
 def display_board
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  display_board(board)
-  puts "   |   |   "
+
+puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
