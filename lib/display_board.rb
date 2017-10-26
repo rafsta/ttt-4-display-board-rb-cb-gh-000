7d9c1853ca15b4f588e81c0e0e1a551bd9041bdf
Raffi Jebejian <raffi.jebejian@gmail.com>
2017-10-26 06:14:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-
+def empty
+  empty = {"1"=" ","2"=" ","3"=" ","4" =" ", "5" = " ", "6" = " ", "7" = " ", "8" = " ", "9" = " "}
+end
 
 def display_board
   puts " 1 | 2 | 3 "
